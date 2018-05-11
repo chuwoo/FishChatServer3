b2a0e5dc8557dde22239f9343e21883047d88edc
harold.miao <harold.miao@vipshop.com>
2018-05-11 15:47:44 +0800

    init

--- cmd/gateway/gateway.go
+++ cmd/gateway/gateway.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"time"
+	"github.com/oikomi/FishChatServer3/cmd/gateway/app/options"
 	"math/rand"
+	"time"
+
+	flag "github.com/spf13/pflag"
+
+	"github.com/golang/glog"
 )
 
 func main() {
+
+	glog.Info()
 	rand.Seed(time.Now().UTC().UnixNano())
 
+	config := options.NewGatewayFlags()
+	config.AddFlags(flag.CommandLine)
+
+	flag.Parse()
 
 }
